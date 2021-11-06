070bc8bc1aedcca9869e6b097704b621d711e2a4
Ghulam Izzul Fuad <ghulam.izzul@ui.ac.id>
2021-11-06 11:41:05 +0700

    Perbaikan CS6 OOP

--- src/GhulamJmartAK/Jmart.java
+++ src/GhulamJmartAK/Jmart.java
@@ -1,8 +1,14 @@
 package GhulamJmartAK;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
-
 /**
  * Write a description of class Jmart here.
  *
@@ -13,7 +19,14 @@ import java.util.List;
 public class Jmart
 {
     public static List<Product> filterByCategory (List<Product>list, ProductCategory category){
-        return null;
+        List<Product> tempHasil = new ArrayList<Product>();
+
+        for(Product temp : list){
+            if(temp.category == category){
+                tempHasil.add(temp);
+            }
+        }
+        return tempHasil;
     }
 
     public static List<Product> filterByPrice (List<Product>list, double minPrice, double maxPrice) {
@@ -39,6 +52,24 @@ public class Jmart
         System.out.println("payment id" + new Payment(-1, -1, -1, null).id);
         System.out.println("payment id" + new Payment(-1, -1, -1, null).id);
         System.out.println("payment id" + new Payment(-1, -1, -1, null).id);
+
+        String filepath = "D:/Praktikum OOP/jmart/src/GhulamJmartAK/randomProductList.json";
+        try {
+            List<Product> list = read(filepath);
+            List<Product> filtered = filterByPrice(list, 13000.0, 15000.0);
+            filtered.forEach(product -> System.out.println(product.price));
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    public static List<Product> read(String filepath) throws FileNotFoundException {
+        Gson gson = new Gson();
+        Type userListType = new TypeToken<ArrayList<Product>>() {
+        }.getType();
+        BufferedReader br = new BufferedReader(new FileReader(filepath));
+        List<Product> returnList = gson.fromJson(br, userListType);
+        return returnList;
     }
 
 
