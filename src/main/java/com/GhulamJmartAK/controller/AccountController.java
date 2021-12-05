1dc73dc59192b72d40431603997f9a60d6321ccd
Ghulam Izzul Fuad <ghulam.izzul@ui.ac.id>
2021-12-05 23:05:38 +0700

    tutam oop 9

--- src/main/java/com/GhulamJmartAK/controller/AccountController.java
+++ src/main/java/com/GhulamJmartAK/controller/AccountController.java
@@ -1,26 +1,23 @@
 package com.GhulamJmartAK.controller;
 import com.GhulamJmartAK.Account;
+import com.GhulamJmartAK.dbjson.JsonTable;
 import com.GhulamJmartAK.Store;
 import com.GhulamJmartAK.dbjson.JsonAutowired;
-import com.GhulamJmartAK.dbjson.JsonTable;
-import com.GhulamJmartAK.dbjson.Serializable;
 import org.springframework.web.bind.annotation.*;
 
-import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.regex.Pattern;
 
-
 @RestController
 @RequestMapping("/account")
-public class AccountController implements BasicGetController {
+public class AccountController implements BasicGetController<Account>{
     public static final String REGEX_EMAIL = "^\\w+([\\.&`~-]?\\w+)*@\\w+([\\.-]?\\w+)+$";
-    public static final String REGEX_PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d][^-\\s]{8,}$";
+    public static final String REGEX_PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d][^-\\s]{7,}$";
     public static final Pattern REGEX_PATTERN_EMAIL = Pattern.compile(REGEX_EMAIL);
     public static final Pattern REGEX_PATTERN_PASSWORD = Pattern.compile(REGEX_PASSWORD);
-    @JsonAutowired(value = Account.class,filepath = "akun.json")
+    @JsonAutowired(value = Account.class,filepath = "Akun.json")
     public static JsonTable<Account> accountTable;
 
     @PostMapping("/login")
@@ -30,19 +27,20 @@ public class AccountController implements BasicGetController {
                     @RequestParam String password
             )
     {
-        for (Account data : accountTable){
-            try{
-                MessageDigest md = MessageDigest.getInstance("MD5");
-                md.update(password.getBytes());
-                byte[] bytes = md.digest();
-                StringBuilder sb = new StringBuilder();
-                for(int i = 0; i < bytes.length; i++){
-                    sb.append(Integer.toString((bytes[i] & 0xff) + 0x100,16).substring(1));
-                }
-                password = sb.toString();
-            }catch (NoSuchAlgorithmException e){
-                e.printStackTrace();
+        try{
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(password.getBytes());
+            byte[] bytes = md.digest();
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < bytes.length; i++){
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100,16).substring(1));
             }
+            password = sb.toString();
+        }catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+        for (Account data : accountTable){
+
             if(data.email.equals(email) && data.password.equals(password)){
                 return data;
             }
@@ -73,8 +71,7 @@ public class AccountController implements BasicGetController {
         }catch (NoSuchAlgorithmException e){
             e.printStackTrace();
         }
-        if(!name.isBlank() || hasilEmail || hasilPassword ||
-                accountTable.stream().anyMatch(account -> account.email.equals(email))){
+        if(!name.isBlank() && hasilEmail && hasilPassword && !accountTable.stream().anyMatch(account -> account.email.equals(email))){
             Account account =  new Account(name, email, password, 0);
             accountTable.add(account);
             return account;
@@ -117,7 +114,8 @@ public class AccountController implements BasicGetController {
     }
 
     @Override
-    public Serializable getById(int id) {
+    @GetMapping("/{id}")
+    public Account getById(@PathVariable int id) {
         return BasicGetController.super.getById(id);
     }
 
--- src/main/java/com/GhulamJmartAK/controller/BasicGetController.java
+++ src/main/java/com/GhulamJmartAK/controller/BasicGetController.java
@@ -1,25 +1,27 @@
 package com.GhulamJmartAK.controller;
 
+import com.GhulamJmartAK.Algorithm;
 import com.GhulamJmartAK.dbjson.JsonTable;
 import com.GhulamJmartAK.dbjson.Serializable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-public interface BasicGetController<T extends Serializable> {
-
+public interface BasicGetController <T extends Serializable> {
     @GetMapping("/{id}")
-    public default T getById(int id) {
-        return getJsonTable().get(id);
+    public default T getById (@PathVariable int id){
+        return Algorithm.<T>find(getJsonTable(),e -> e.id == id);
     }
 
-    public abstract JsonTable<T> getJsonTable();
+    public abstract JsonTable<T> getJsonTable ();
 
     @GetMapping("/page")
-    public default List<T> getPage(int page, int pageSize) {
-        return getJsonTable().subList(page, pageSize);
+    public default List<T> getPage(int page, int pageSize){
+        final JsonTable<T> table = getJsonTable();
+        return Algorithm.paginate(table,page,pageSize,o->true);
     }
 
 }
